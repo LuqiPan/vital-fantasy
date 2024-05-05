d996227bce40e56bc8d038cfc6fee1351ce69ffc
Luqi Pan <github@luqipan.com>
2024-05-05 13:23:19 -0700

    feat: implement parseStats function

--- packages/parse/src/lib/parse.spec.ts
+++ packages/parse/src/lib/parse.spec.ts
@@ -1,7 +1,16 @@
-import { parse } from './parse';
+import { parseStats } from './parse';
+import { Readable } from 'stream';
 
-describe('parse', () => {
-  it('should work', () => {
-    expect(parse()).toEqual('parse');
+const rStream = Readable.from(`
+Rider,Number,Bike,"Fort William - 2024 Class","Fort William - 2024 Cost","Fort William - 2024 Status","Mont-Sainte-Anne - 2023 Class","Mont-Sainte-Anne - 2023 Position","Mont-Sainte-Anne - 2023 Points","Snowshoe - 2023 Class","Snowshoe - 2023 Position","Snowshoe - 2023 Points","Les Gets - 2023 Class","Les Gets - 2023 Position","Les Gets - 2023 Points","Loudenvielle - 2023 Class","Loudenvielle - 2023 Position","Loudenvielle - 2023 Points","Andorra - 2023 Class","Andorra - 2023 Position","Andorra - 2023 Points","Val di Sole - 2023 Class","Val di Sole - 2023 Position","Val di Sole - 2023 Points","Leogang - 2023 Class","Leogang - 2023 Position","Leogang - 2023 Points","Lenzerheide - 2023 Class","Lenzerheide - 2023 Position","Lenzerheide - 2023 Points"
+"Abigail Hogie",,"Beyond Racing","Elite Women",14000,,,,,,,,,,,,,,,,,,,,,,,,,
+`);
+
+describe('parseStats', () => {
+  it('should work', async () => {
+    const stats = await parseStats(rStream);
+    expect(stats).toEqual({
+      'Abigail Hogie|Elite Women': { cost: 14000 },
+    });
   });
 });
--- packages/parse/src/lib/parse.ts
+++ packages/parse/src/lib/parse.ts
@@ -1,3 +1,54 @@
-export function parse(): string {
-  return 'parse';
+import { finished } from 'node:stream/promises';
+const csvParser = require('csv-parser');
+
+class Rider {
+  private name: string;
+  private class: string;
+
+  constructor(name: string, classs: string) {
+    this.name = name;
+    this.class = classs;
+  }
+
+  public toString(): string {
+    return `${this.name}|${this.class}`;
+  }
+}
+
+type Stat = {
+  cost: number;
+};
+
+type Stats = {
+  [key: string]: Stat;
+};
+
+export async function parseStats(
+  rstream: NodeJS.ReadableStream
+): Promise<Stats> {
+  var stats: Stats = {};
+  var firstRow = true;
+
+  rstream
+    .pipe(
+      csvParser({
+        mapHeaders: (header: string) =>
+          header.replaceAll(' ', '').replaceAll('-', ''),
+      })
+    )
+    .on('data', (row: any) => {
+      if (firstRow) {
+        firstRow = false;
+        return;
+      }
+
+      const rider = new Rider(row['_0'], row['_3']);
+
+      stats[rider.toString()] = {
+        cost: Number(row['_4']),
+      };
+    });
+
+  await finished(rstream);
+  return stats;
 }
