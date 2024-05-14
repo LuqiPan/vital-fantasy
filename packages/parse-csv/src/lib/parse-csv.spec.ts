a43793e3646debc31e4a84bb2d69aafaebb3b3f2
Luqi Pan <github@luqipan.com>
2024-05-13 21:14:23 -0700

    test: refine parseCsv test

--- packages/parse-csv/src/lib/parse-csv.spec.ts
+++ packages/parse-csv/src/lib/parse-csv.spec.ts
@@ -1,11 +1,18 @@
 import { parseCsv } from './parse-csv';
 import { Readable } from 'stream';
 
-const rStream = Readable.from(`Rider,Class
-Abigail Hogie,Elite Women`);
+const rStream =
+  Readable.from(`Rider,Number,Bike,"Fort William - 2024 Class","Fort William - 2024 Cost","Fort William - 2024 Status","Mont-Sainte-Anne - 2023 Class","Mont-Sainte-Anne - 2023 Position","Mont-Sainte-Anne - 2023 Points","Snowshoe - 2023 Class","Snowshoe - 2023 Position","Snowshoe - 2023 Points","Les Gets - 2023 Class","Les Gets - 2023 Position","Les Gets - 2023 Points","Loudenvielle - 2023 Class","Loudenvielle - 2023 Position","Loudenvielle - 2023 Points","Andorra - 2023 Class","Andorra - 2023 Position","Andorra - 2023 Points","Val di Sole - 2023 Class","Val di Sole - 2023 Position","Val di Sole - 2023 Points","Leogang - 2023 Class","Leogang - 2023 Position","Leogang - 2023 Points","Lenzerheide - 2023 Class","Lenzerheide - 2023 Position","Lenzerheide - 2023 Points"
+"Abigail Hogie",,"Beyond Racing","Elite Women",14000,,,,,,,,,,,,,,,,,,,,,,,,,`);
 
 describe('parseCsv', () => {
   it('should work', async () => {
-    expect(await parseCsv(rStream)).toEqual([['Abigail Hogie', 'Elite Women']]);
+    const rows = await parseCsv(rStream);
+    expect(rows.length).toEqual(1);
+
+    const row = rows[0];
+    expect(row[0]).toEqual('Abigail Hogie');
+    expect(row[3]).toEqual('Elite Women');
+    expect(Number(row[4])).toEqual(14000);
   });
 });
