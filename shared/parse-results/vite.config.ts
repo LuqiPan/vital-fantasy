33f34be27c1f740e2cf6f570dd4f308b90807bc1
Luqi Pan <github@luqipan.com>
2024-05-14 20:53:23 -0700

    test: migrate from jest to vitest in parse-results

---
+++ shared/parse-results/vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vitest/config';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/shared/parse-results',
+
+  plugins: [nxViteTsPaths()],
+
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      enabled: true,
+      reportsDirectory: '../../coverage/shared/parse-results',
+      provider: 'istanbul',
+    },
+  },
+});
