62ce995fb30a3f9ad23272fedf33676a97f8fffd
Luqi Pan <github@luqipan.com>
2024-05-15 21:13:42 -0700

    feat: adopt parseFiles in ft-bill app

--- apps/ft-bill/src/main.ts
+++ apps/ft-bill/src/main.ts
@@ -1,26 +1,15 @@
-import { parseStats } from '@vital-fantasy/parse-stats';
-import { parseResults } from '@vital-fantasy/parse-results';
-import { combine } from '@vital-fantasy/combine';
+import { parseFiles } from '@vital-fantasy/parse-files';
 
 (async () => {
-  try {
-    const fs = require('fs');
+  const statsCsvPath =
+    'apps/ft-bill/src/assets/Fort William Advanced Stats.csv';
 
-    const statsCsv = fs.createReadStream(
-      'apps/ft-bill/src/assets/Fort William Advanced Stats.csv'
-    );
-    const stats = await parseStats(statsCsv);
+  const resultsCsvPath = 'apps/ft-bill/src/assets/ft-bill-results.csv';
 
-    const resultsCsv = fs.createReadStream(
-      'apps/ft-bill/src/assets/ft-bill-results.csv'
-    );
-    const results = await parseResults(resultsCsv);
-
-    const { predictions, outliers } = combine(stats, results);
-    console.log(predictions);
-    console.log(outliers);
-  } catch (e) {
-    console.log('ERROR', e);
-    process.exit(1);
-  }
+  const { predictions, outliers } = await parseFiles(
+    statsCsvPath,
+    resultsCsvPath
+  );
+  console.log(predictions);
+  console.log(outliers);
 })();
