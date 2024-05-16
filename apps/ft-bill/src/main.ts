9d216a96fcecf20ac6ad581d6d4fe6ed09a49eeb
Luqi Pan <github@luqipan.com>
2024-05-15 20:39:52 -0700

    feat: naive implementation that parse files

--- apps/ft-bill/src/main.ts
+++ apps/ft-bill/src/main.ts
@@ -1,3 +1,26 @@
-import { combine } from "@vital-fantasy/combine";
+import { parseStats } from '@vital-fantasy/parse-stats';
+import { parseResults } from '@vital-fantasy/parse-results';
+import { combine } from '@vital-fantasy/combine';
 
-console.log('Hello World');
+(async () => {
+  try {
+    const fs = require('fs');
+
+    const statsCsv = fs.createReadStream(
+      'apps/ft-bill/src/assets/Fort William Advanced Stats.csv'
+    );
+    const stats = await parseStats(statsCsv);
+
+    const resultsCsv = fs.createReadStream(
+      'apps/ft-bill/src/assets/ft-bill-results.csv'
+    );
+    const results = await parseResults(resultsCsv);
+
+    const { predictions, outliers } = combine(stats, results);
+    console.log(predictions);
+    console.log(outliers);
+  } catch (e) {
+    console.log('ERROR', e);
+    process.exit(1);
+  }
+})();
