41cae4142c6932006e16622a88336fa5b66b5ed8
Luqi Pan <github@luqipan.com>
2024-05-15 17:42:54 -0700

    test: add another test case to improve coverage

--- shared/combine/src/lib/combine.spec.ts
+++ shared/combine/src/lib/combine.spec.ts
@@ -7,9 +7,38 @@ describe('combine', () => {
   it.each([
     [{}, {}, new ReturnValues()],
     [{ 'Abigail Hogie|Elite Women': { cost: 14000 } }, {}, new ReturnValues()],
-    [{}, { 'Abigail Hogie|Elite Women': { semiRank: 1 } }, new ReturnValues({}, [['Abigail Hogie|Elite Women', '{"semiRank":1}']])],
-    [{ 'Abigail Hogie|Not Real Class': { cost: 14000 } }, { 'Abigail Hogie|Not Real Class': { semiRank: 1 } }, new ReturnValues({}, [['Abigail Hogie|Not Real Class', '{"semiRank":1}']])],
-    [{ 'Abigail Hogie|Elite Women': { cost: 14000 } }, { 'Abigail Hogie|Elite Women': { semiRank: 1 } }, new ReturnValues({'Abigail Hogie|Elite Women': {PointsPerCost: pointsBreakdown['Elite Women'][0]/14000}})],
+    [
+      {},
+      { 'Abigail Hogie|Elite Women': { semiRank: 1 } },
+      new ReturnValues({}, [['Abigail Hogie|Elite Women', '{"semiRank":1}']]),
+    ],
+    [
+      {},
+      {
+        'Abigail Hogie|Elite Women': { semiRank: 1 },
+        'Someone Else|Not Real Class': { semiRank: 1 },
+      },
+      new ReturnValues({}, [
+        ['Abigail Hogie|Elite Women', '{"semiRank":1}'],
+        ['Someone Else|Not Real Class', '{"semiRank":1}'],
+      ]),
+    ],
+    [
+      { 'Abigail Hogie|Not Real Class': { cost: 14000 } },
+      { 'Abigail Hogie|Not Real Class': { semiRank: 1 } },
+      new ReturnValues({}, [
+        ['Abigail Hogie|Not Real Class', '{"semiRank":1}'],
+      ]),
+    ],
+    [
+      { 'Abigail Hogie|Elite Women': { cost: 14000 } },
+      { 'Abigail Hogie|Elite Women': { semiRank: 1 } },
+      new ReturnValues({
+        'Abigail Hogie|Elite Women': {
+          PointsPerCost: pointsBreakdown['Elite Women'][0] / 14000,
+        },
+      }),
+    ],
   ])(
     'combine(%s, %s) -> %s',
     (stats: Stats, results: Results, expected: ReturnValues) => {
