18a6d56f8f19085439b3074aaa2f2f3cb511acee
Luqi Pan <github@luqipan.com>
2024-05-17 08:48:24 -0700

    test: add another test for coverage
    ... for lulz

--- libs/parse-csv/src/lib/parse-csv.spec.ts
+++ libs/parse-csv/src/lib/parse-csv.spec.ts
@@ -2,10 +2,9 @@ import { parseCsv } from './parse-csv';
 import { Readable } from 'stream';
 import { sampleCsv } from '@vital-fantasy/constants';
 
-const rStream = Readable.from(sampleCsv);
-
 describe('parseCsv', () => {
   it('should work', async () => {
+    const rStream = Readable.from(sampleCsv);
     const rows = await parseCsv(rStream);
     expect(rows.length).toEqual(1);
 
@@ -13,5 +12,16 @@ describe('parseCsv', () => {
     expect(row[0]).toEqual('Abigail Hogie');
     expect(row[3]).toEqual('Elite Women');
     expect(Number(row[4])).toEqual(14000);
+  });
+
+  it('log error when there is one', async () => {
+    const consoleSpy = vi.spyOn(console, 'error');
+    const rStream = Readable.from(`lol
+    one,two`);
+
+    const rows = await parseCsv(rStream);
+    expect(rows.length).toEqual(0);
+
+    expect(consoleSpy).toHaveBeenCalledOnce();
   });
 });
