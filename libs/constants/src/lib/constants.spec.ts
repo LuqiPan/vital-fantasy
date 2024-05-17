fdbc03a4f65a7463a974127c3e88a81c491c43ec
Luqi Pan <github@luqipan.com>
2024-05-16 17:58:57 -0700

    chore: move constants from shared to libs

--- libs/constants/src/lib/constants.spec.ts
+++ libs/constants/src/lib/constants.spec.ts
@@ -2,6 +2,6 @@ import { sampleCsv } from './constants';
 
 describe('constants', () => {
   it('should work', () => {
-    expect(sampleCsv.indexOf("\n")).not.toEqual(-1);
+    expect(sampleCsv.indexOf('\n')).not.toEqual(-1);
   });
 });
