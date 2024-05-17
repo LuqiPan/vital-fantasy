c50fabcda7f66a1999bf835a6c1c6da86916c294
Luqi Pan <github@luqipan.com>
2024-05-16 21:15:42 -0700

    fix: make await explicit

--- apps/ft-bill/src/main.ts
+++ apps/ft-bill/src/main.ts
@@ -3,4 +3,4 @@ import { analyzeRound } from '@vital-fantasy/analyze-round';
 const statsCsvPath = 'apps/ft-bill/src/assets/Fort William Advanced Stats.csv';
 const resultsCsvPath = 'apps/ft-bill/src/assets/ft-bill-results.csv';
 
-analyzeRound('Fort William', statsCsvPath, resultsCsvPath);
+await analyzeRound('Fort William', statsCsvPath, resultsCsvPath);
